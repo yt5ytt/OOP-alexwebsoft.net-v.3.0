e02ffbe99daa25279e840b906c3b4447d1f60d3e
yt5ytt <yt5ytt@gmail.com>
2021-05-05 00:11:35 +0200

    First commit

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
    */
 include('header.php');
 
-  echo 'OK. We\'re started to make another version of portfolio web site';
+  echo 'OK. We\'re started to make another version of portfolio web site<br />';
+
+  echo 'Dodajmo ovu liniju';
 
 include('footer.php');
