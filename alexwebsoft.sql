f7eea6d56d869a4e403276816fb6aca1f20f3e9d
yt5ytt <alexwebsoft.net@gmail.com>
2021-05-05 22:13:05 +0200

    Linux adding

---
+++ alexwebsoft.sql
@@ -0,0 +1,104 @@
+-- MariaDB dump 10.17  Distrib 10.4.11-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: alexwebsoft
+-- ------------------------------------------------------
+-- Server version	10.4.11-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `pages`
+--
+
+DROP TABLE IF EXISTS `pages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pages` (
+  `id` int(2) NOT NULL AUTO_INCREMENT,
+  `title` varchar(20) CHARACTER SET utf8 DEFAULT NULL,
+  `link` varchar(20) CHARACTER SET utf8 DEFAULT NULL,
+  `status` varchar(20) CHARACTER SET utf8 DEFAULT 'Main',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pages`
+--
+
+LOCK TABLES `pages` WRITE;
+/*!40000 ALTER TABLE `pages` DISABLE KEYS */;
+INSERT INTO `pages` VALUES (1,'Home','index.php','Main'),(2,'Portfolio','portfolio.inc.php','Main'),(3,'Contact Us','contactUs.inc.php','Main'),(4,'WordPress','','Main'),(5,'Themes','wpThemes.inc.php','Sub'),(6,'Plugins','wpPlugins.inc.php','Sub');
+/*!40000 ALTER TABLE `pages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `social`
+--
+
+DROP TABLE IF EXISTS `social`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `social` (
+  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `title` varchar(30) NOT NULL,
+  `icon` varchar(100) NOT NULL,
+  `link` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `social`
+--
+
+LOCK TABLES `social` WRITE;
+/*!40000 ALTER TABLE `social` DISABLE KEYS */;
+INSERT INTO `social` VALUES (1,'FaceBook','facebook','https://www.facebook.com/alex.yt5ytt'),(2,'LinkedIn','linkedin','https://www.linkedin.com/in/aleksandar-safranec-0743b48b/'),(3,'GitHub','github','https://github.com/yt5ytt');
+/*!40000 ALTER TABLE `social` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `technologies`
+--
+
+DROP TABLE IF EXISTS `technologies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `technologies` (
+  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `title` varchar(20) NOT NULL,
+  `file` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `technologies`
+--
+
+LOCK TABLES `technologies` WRITE;
+/*!40000 ALTER TABLE `technologies` DISABLE KEYS */;
+INSERT INTO `technologies` VALUES (1,'HTML5','html5.jpg'),(2,'CSS3','css3.jpg'),(3,'PHP','php.jpg'),(4,'MySQL','mysql.jpg'),(5,'javaScript','js.jpg'),(6,'WordPress','wp.jpg');
+/*!40000 ALTER TABLE `technologies` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-05-05 22:12:05
--- inc/topLine.inc.php
+++ inc/topLine.inc.php
@@ -1,15 +1,16 @@
-<?php
+<div id="topLine">
 
- ?>
+  <div class="wrapper">
 
- <div id="topLine">
-   <div class="wrapper">
-     <div id="contactInfo">
-       <i class="fas fa-mobile-alt"></i> &nbsp; 381.60.599.1001 &nbsp; &nbsp; &nbsp;
-       <i class="fas fa-envelope"></i> &nbsp; alexwebsoft.net@gmail.com
-     </div>
-     <nav>
-       <?php //include("mainMenu.php"); ?>
-     </nav>
-   </div>
- </div><!--kraj diva #topLine-->
+    <div id="contactInfo">
+
+      <i class="fas fa-mobile-alt"></i> &nbsp; 381.60.599.1001 &nbsp; &nbsp; &nbsp;
+      <i class="fas fa-envelope"></i> &nbsp; alexwebsoft.net@gmail.com
+
+    </div><!-- kraj diva .contactInfo -->
+
+    <?php include(ABSPATH . 'inc' . DS . 'mainMenu.inc.php'); ?>
+
+  </div><!-- kraj diva .wrapper -->
+
+</div><!--kraj diva #topLine-->
